c4440fcef67ab7e88b60b844e6a7e0bc7134ca5f
hadirch <hadirich3@gmail.com>
2025-01-24 08:35:21 +0700

    fix: undo cors problem

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -16,8 +16,8 @@ class CorsMiddleware
     public function handle(Request $request, Closure $next): Response
     {
         $response = $next($request);
-        // $response->headers->set('Access-Control-Allow-Origin', 'http://localhost:5173');
-        $response->headers->set('Access-Control-Allow-Origin', 'https://final-project-frontend-hwv5oky06-hadirchs-projects.vercel.app/');
+        $response->headers->set('Access-Control-Allow-Origin', 'http://localhost:5173');
+        // $response->headers->set('Access-Control-Allow-Origin', 'https://final-project-frontend-hwv5oky06-hadirchs-projects.vercel.app/');
         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
         return $response;
