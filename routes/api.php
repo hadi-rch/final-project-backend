9072ecec6c772ef43ae66aebf55d59f5214d735f
hadirch <hadirich3@gmail.com>
2025-01-24 21:45:54 +0700

    new

--- routes/api.php
+++ routes/api.php
@@ -23,7 +23,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
     Route::post('pay', [PaymentController::class, 'createTransaction'])->middleware(['auth:api', 'isverified']);
-    Route::get('order', [OrdersController::class, 'show'])->middleware(['auth:api', 'isverified']);
+    Route::get('orders', [OrdersController::class, 'show'])->middleware(['auth:api', 'isverified']);
     Route::apiResource('order', OrdersController::class, ['index','delete'])->middleware(['auth:api', 'isadmin']);
     Route::apiResource('category', CategoriesController::class);
     Route::apiResource('product', ProductsController::class);
